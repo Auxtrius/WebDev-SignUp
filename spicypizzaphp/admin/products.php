505b9dc23a8b61aaa2223331a9beb7605ac816d1
Ralph <raaapido27@gmail.com>
2023-10-24 23:38:26 +0800

    Initial commit and changes to the files

---
+++ spicypizzaphp/admin/products.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    $title = 'Products';
+    $products = 'active';
+    require_once('../includes/head.php')
+?>
+<body>
+<?php
+    require_once('../includes/header.admin.php')
+?>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+            <?php
+                require_once('../includes/sidebar.php')
+            ?>
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-light">
+                    <h2 class="h3 brand-color pt-3 pb-2">Product</h2>
+                    <div class="table-responsive overflow-hidden">
+                        <div class="row g-2 mb-2 m-0">
+                            <div id="MyButtons" class="d-flex mb-md-2 mb-lg-0 col-12 col-md-auto"></div>
+                            <div class="form-group col-12 col-sm-auto flex-sm-grow-1 flex-lg-grow-0 ms-lg-auto">
+                                <select name="product-category" id="product-category" class="form-select me-md-2">
+                                    <option value="">All Category</option>
+                                    <option value="Pizza">Pizza</option>
+                                    <option value="Refreshments">Refreshments</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-12 col-sm-auto flex-sm-grow-1 flex-lg-grow-0">
+                                <select name="product-status" id="product-status" class="form-select me-md-2">
+                                    <option value="">All Status</option>
+                                    <option value="Available">Available</option>
+                                    <option value="Soldout">Soldout</option>
+                                </select>
+                            </div>
+                            <div class="search-keyword col-12 flex-lg-grow-0 d-flex">
+                                <div class="input-group">
+                                    <input type="text" name="keyword" id="keyword" placeholder="Search Product" class="form-control">
+                                    <button class="btn btn-outline-secondary brand-bg-color" type="button"><i class="fa fa-search color-white" aria-hidden="true"></i></button>
+                                </div>
+                                <button class="btn btn-outline-secondary btn-add" type="button"><i class="fa fa-plus brand-color" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+                        <table id="products" class="table table-striped table-sm">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Product Name</th>
+                                    <th scope="col">Category</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Availability</th>
+                                </tr>
+                            </thead>
+                            <?php
+                                $productArray = array(
+                                    array(
+                                        'name' => 'Spicy Pizza with Jalapenos',
+                                        'category' => 'Pizza',
+                                        'price' => 'P 400.00',
+                                        'availability' => 'Available',
+                                    ),
+                                    array(
+                                        'name' => 'Matcha Frappe',
+                                        'category' => 'Refreshment',
+                                        'price' => 'P 125.00',
+                                        'availability' => 'Available',
+                                    ),
+                                    array(
+                                        'name' => 'Spicy Margherita Pizza',
+                                        'category' => 'Pizza',
+                                        'price' => 'P 625.00',
+                                        'availability' => 'Soldout',
+                                    ),
+                                    array(
+                                        'name' => 'Cucumber Lemonade',
+                                        'category' => 'Refreshment',
+                                        'price' => 'P 95.00',
+                                        'availability' => 'Soldout',
+                                    ),
+                                    array(
+                                        'name' => 'Spicy Veggie Delight Pizza',
+                                        'category' => 'Pizza',
+                                        'price' => 'P 600.00',
+                                        'availability' => 'Available',
+                                    ),
+                                    array(
+                                        'name' => 'Spicy Meat Lovers Pizza',
+                                        'category' => 'Pizza',
+                                        'price' => 'P 550.00',
+                                        'availability' => 'Available',
+                                    ),
+                                    array(
+                                        'name' => 'Strawberry Creamcheese',
+                                        'category' => 'Refreshment',
+                                        'price' => 'P 225.00',
+                                        'availability' => 'Available',
+                                    ),
+                                    array(
+                                        'name' => 'Spicy Hawaiian Pizza',
+                                        'category' => 'Pizza',
+                                        'price' => 'P 450.00',
+                                        'availability' => 'Soldout',
+                                    ),
+                                    array(
+                                        'name' => 'Lychee Soda',
+                                        'category' => 'Refreshment',
+                                        'price' => 'P 95.00',
+                                        'availability' => 'Soldout',
+                                    ),
+                                    array(
+                                        'name' => 'Spicy BBQ Chicken Pizza',
+                                        'category' => 'Pizza',
+                                        'price' => 'P 360.00',
+                                        'availability' => 'Available',
+                                    )
+                                );
+                            ?>
+                            <tbody>
+                                <?php
+                                $counter = 1;
+                                foreach ($productArray as $item){
+                                ?>
+                                    <tr>
+                                        <td><?= $counter ?></td>
+                                        <td><?= $item['name'] ?></td>
+                                        <td><?= $item['category'] ?></td>
+                                        <td><?= $item['price'] ?></td>
+                                        <td><?= $item['availability'] ?></td>
+                                        <td class="text-center"><a href=""><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
+                                    </tr>
+                                <?php
+                                    $counter++;
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </main>
+            </div>
+        </div>
+    </main>
+    <?php
+        require_once('../includes/js.php')
+    ?>
+    <script src="../js/products.js"></script>
+</body>
+</html>
